a990a1283f3e180783d5c76e74a228baf3b3d329
DeshnaBera <beradeshna@gmail.com>
2024-01-15 13:27:02 +0530

    upload artifacts

--- list_untag.py
+++ list_untag.py
@@ -1,7 +1,11 @@
 import boto3
 
 def list_untagged_s3_buckets():
-    s3 = boto3.client('s3')
+    session = boto3.Session(
+        aws_access_key_id='AKIAXCMHLBE24P6BZKOI',
+        aws_secret_access_key='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'
+    )
+    s3 = session.client('s3')
     response = s3.list_buckets()
     untagged_buckets = []
     for bucket in response['Buckets']:
