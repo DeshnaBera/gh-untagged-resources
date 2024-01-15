8bb19f862733f91f6066f22ec6e7831bdd82e2f1
DeshnaBera <beradeshna@gmail.com>
2024-01-15 13:30:06 +0530

    list lambda

--- list_untag.py
+++ list_untag.py
@@ -1,15 +1,11 @@
 import boto3
 
-def list_untagged_s3_buckets():
-    session = boto3.Session(
-        aws_access_key_id='AKIAXCMHLBE24P6BZKOI',
-        aws_secret_access_key='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'
-    )
-    s3 = session.client('s3')
-    response = s3.list_buckets()
-    untagged_buckets = []
-    for bucket in response['Buckets']:
-        tags = s3.get_bucket_tagging(Bucket=bucket['Name'])
-        if 'TagSet' not in tags:
-            untagged_buckets.append(bucket['Name'])
-    return untagged_buckets
+def list_untagged_lambda_functions():
+    lambda_client = boto3.client('lambda')
+    response = lambda_client.list_functions()
+    untagged_functions = []
+    for function in response['Functions']:
+        tags = lambda_client.list_tags(Resource=function['FunctionArn'])
+        if 'Tags' not in tags:
+            untagged_functions.append(function['FunctionName'])
+    return untagged_functions
